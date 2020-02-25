fdfdf389090d3d5e8a6cd1343a1528a7f068dc1b
treeandgrass <mymymymypcy@163.com>
2020-02-25 14:52:16 +0800

    feat: simhash

---
+++ bigdata/simhash.js
@@ -0,0 +1,89 @@
+function murmurhash3_32_gc(key, seed) {
+	var remainder, bytes, h1, h1b, c1, c1b, c2, c2b, k1, i;
+	
+	remainder = key.length & 3; // key.length % 4
+	bytes = key.length - remainder;
+	h1 = seed;
+	c1 = 0xcc9e2d51;
+	c2 = 0x1b873593;
+	i = 0;
+	
+	while (i < bytes) {
+	  	k1 = 
+	  	  ((key.charCodeAt(i) & 0xff)) |
+	  	  ((key.charCodeAt(++i) & 0xff) << 8) |
+	  	  ((key.charCodeAt(++i) & 0xff) << 16) |
+	  	  ((key.charCodeAt(++i) & 0xff) << 24);
+		++i;
+		
+		k1 = ((((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16))) & 0xffffffff;
+		k1 = (k1 << 15) | (k1 >>> 17);
+		k1 = ((((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16))) & 0xffffffff;
+
+		h1 ^= k1;
+        h1 = (h1 << 13) | (h1 >>> 19);
+		h1b = ((((h1 & 0xffff) * 5) + ((((h1 >>> 16) * 5) & 0xffff) << 16))) & 0xffffffff;
+		h1 = (((h1b & 0xffff) + 0x6b64) + ((((h1b >>> 16) + 0xe654) & 0xffff) << 16));
+	}
+	
+	k1 = 0;
+	
+	switch (remainder) {
+		case 3: k1 ^= (key.charCodeAt(i + 2) & 0xff) << 16;
+		case 2: k1 ^= (key.charCodeAt(i + 1) & 0xff) << 8;
+		case 1: k1 ^= (key.charCodeAt(i) & 0xff);
+		
+		k1 = (((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16)) & 0xffffffff;
+		k1 = (k1 << 15) | (k1 >>> 17);
+		k1 = (((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16)) & 0xffffffff;
+		h1 ^= k1;
+	}
+	
+	h1 ^= key.length;
+
+	h1 ^= h1 >>> 16;
+	h1 = (((h1 & 0xffff) * 0x85ebca6b) + ((((h1 >>> 16) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
+	h1 ^= h1 >>> 13;
+	h1 = ((((h1 & 0xffff) * 0xc2b2ae35) + ((((h1 >>> 16) * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
+	h1 ^= h1 >>> 16;
+
+	return h1 >>> 0;
+}
+
+const seek =  ~~(Math.random() * 2 ^ 32);
+const weightsTable = [];
+for(let i = 0; i < 32; i++) {
+    weightsTable[i] = 1 + ~~(Math.random() * 5);
+}
+
+function simhash(str) {
+    const chunks =  str.split(" ");
+    const stack = [];
+    chunks.forEach((element, i) => {
+        const hashCode = murmurhash3_32_gc(element, seek);
+        const represent = hashCode.toString(2);
+
+        for (let j = 0; j < 32; j++) {
+            let code = +represent[j] > 0 ? 1 : -1;
+            code *= weightsTable[j];
+            stack[j] = (stack[j] || 0) + code;
+        }
+    });
+
+    for(let i = 0; i < stack.length; i++) {
+        stack[i] = stack[i] > 0 ? 1 : 0;
+    }
+
+    return stack.join('');
+}
+
+const str1 = 'hello, world;'
+const str2 = 'good, day';
+const str3 = 'hello, sunny day';
+const str4 = 'hello, downpour';
+
+
+console.log(simhash(str1));
+console.log(simhash(str2));
+console.log(simhash(str3));
+console.log(simhash(str4));
